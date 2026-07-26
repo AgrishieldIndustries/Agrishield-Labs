bdb70611d7acc0e8dc8d8b86cddd0c68680e81e9
AgrishieldIndustries <info.agrishield@gmail.com>
2026-07-26 10:29:27 +0530

    style: fix Pratibha Patil photo aspect ratio and alignment on about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -386,7 +386,7 @@ export default function About() {
               </div>
 
               <div className="lg:col-span-5 lg:order-1 flex justify-center">
-                <div className="relative w-full max-w-[380px] aspect-[4/3] mr-4 mb-4">
+                <div className="relative w-full max-w-[300px] aspect-[3/4] mr-4 mb-4">
                   {/* Red backdrop offset */}
                   <div className="absolute inset-0 bg-[#b91c1c] rounded-2xl translate-x-3 translate-y-3" />
                   {/* Image card */}
@@ -394,7 +394,7 @@ export default function About() {
                     <img
                       src="/mrs-pratibha-patil.png"
                       alt="Mrs. Pratibha Patil, HRD Director of Agrishield Labs Pvt. Ltd."
-                      className="w-full h-full object-cover"
+                      className="w-full h-full object-cover object-top"
                       loading="lazy"
                     />
                   </div>
